f3773c8453d44e6df96153f6960e0f433c64e728
David Ralph <davetheprogrammer2@gmail.com>
2018-12-03 21:04:11 +0000

    Update custom.js

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -10,19 +10,19 @@
 MIT License
 Github: https://github.com/ohlookitsderpy/derpyenterprises.org
 */
-
-var easter_egg = new Konami(function() { //why are you looking here, you ruined the easter egg >:(
-  var uwu = new Audio('assets/audio/kirbe.ogg').play();
-  alert("konami code activated, click ok to enable kirbe mode");
-  document.body.style.background = "url('assets/img/kirby.jpg')";
-  document.body.style.color = "white";
-  document.title = "kirbe mode enabled";
-  document.getElementById("icon").src = "assets/img/kirby.gif";
-  document.getElementById("titletext").innerHTML = "Kirby <3";
-  document.getElementById("e").innerHTML = "Enjoyed this Easter egg? Star this repo on GitHub <a href='https://github.com/ohlookitsderpy/derpyenterprises.org' target=_blank'>here</a>!";
-  document.getElementById("o").innerHTML = "The page will automatically refresh once the song is over!"
-  document.getElementById("uwu").style.display = "none";
-  uwu.onended = function() {
-      location.reload();
-  };
-});
+var egg = new Egg("up,up,down,down,left,right,left,right,b,a", function () { //why are you looking here, you ruined the easter egg >:(
+    var uwu = new Audio('assets/audio/kirbe.ogg');
+    uwu.play();
+    alert("konami code activated, click ok to enable kirbe mode");
+    document.body.style.background = "url('assets/img/kirby.jpg')";
+    document.body.style.color = "white";
+    document.title = "kirbe mode enabled";
+    document.getElementById("icon").src = "assets/img/kirby.gif";
+    document.getElementById("titletext").innerHTML = "Kirby <3";
+    document.getElementById("e").innerHTML = "Enjoyed this Easter egg? Star this repo on GitHub <a href='https://github.com/ohlookitsderpy/derpyenterprises.org' target=_blank'>here</a>!";
+    document.getElementById("o").innerHTML = "The page will automatically refresh once the song is over!"
+    document.getElementById("uwu").style.display = "none";
+    uwu.onended = function() {
+        location.reload();
+    };
+}).listen();
