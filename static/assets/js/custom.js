6ae1342ad8fe942ae9704f1916182f156336650a
David Ralph <davetheprogrammer2@gmail.com>
2018-12-08 13:32:37 +0000

    Update custom.js

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -10,19 +10,31 @@
 MIT License
 Github: https://github.com/ohlookitsderpy/derpyenterprises.org
 */
-var egg = new Egg("up,up,down,down,left,right,left,right,b,a", function () { //why are you looking here, you ruined the easter egg >:(
-    var uwu = new Audio('assets/audio/kirbe.ogg');
-    uwu.play();
-    alert("konami code activated, click ok to enable kirbe mode");
-    document.body.style.background = "url('assets/img/kirby.jpg')";
-    document.body.style.color = "white";
-    document.title = "kirbe mode enabled";
-    document.getElementById("icon").src = "assets/img/kirby.gif";
-    document.getElementById("titletext").innerHTML = "Kirby <3";
-    document.getElementById("e").innerHTML = "Enjoyed this Easter egg? Star this repo on GitHub <a href='https://github.com/ohlookitsderpy/derpyenterprises.org' target=_blank'>here</a>!";
-    document.getElementById("o").innerHTML = "The page will automatically refresh once the song is over!"
-    document.getElementById("uwu").style.display = "none";
-    uwu.onended = function() {
-        location.reload();
-    };
-}).listen();
+// Konami code
+function onKonamiCode(cb) { //https://stackoverflow.com/a/45576888
+  var input = '';
+  var key = '38384040373937396665';
+  document.addEventListener('keydown', (e) => {
+    input += ("" + e.keyCode);
+    if (input === key) return cb();
+    if (!key.indexOf(input)) return;
+    input = ("" + e.keyCode);
+  });
+}
+// Easter Egg
+onKonamiCode(() => {
+  var uwu = new Audio('assets/audio/kirbe.ogg');
+  uwu.play();
+  alert("konami code activated, click ok to enable kirbe mode");
+  document.body.style.background = "url('assets/img/kirby.jpg')";
+  document.body.style.color = "white";
+  document.title = "kirbe mode enabled";
+  document.getElementById("icon").src = "assets/img/kirby.gif";
+  document.getElementById("titletext").innerHTML = "Kirby <3";
+  document.getElementById("e").innerHTML = "Enjoyed this Easter egg? Star this repo on GitHub <a href='https://github.com/ohlookitsderpy/derpyenterprises.org' target=_blank'>here</a>!";
+  document.getElementById("o").innerHTML = "The page will automatically refresh once the song is over!"
+  document.getElementById("uwu").style.display = "none";
+  uwu.onended = () => {
+    location.reload();
+  };
+});
