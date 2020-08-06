5363173fbb94ef039036739dc8039fae5f08e1c1
David Ralph <ohlookitsderpy@protonmail.com>
2020-08-06 11:41:55 +0100

    stuff

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,22 +11,17 @@ MIT License
 GitHub: https://github.com/ohlookitsderpy/derpyenterprises.org
 */
 
-// Konami code
-const onKonamiCode = (cb) => { // https://stackoverflow.com/a/45576888
-  let input = '';
-  let key = '38384040373937396665';
-  document.addEventListener('keydown', (e) => {
-    input += ('' + e.keyCode);
-    if (input === key) return cb();
-    if (!key.indexOf(input)) return;
-    input = ('' + e.keyCode);
-  });
-}
+const instance = new Typewriter('#text', {
+  strings: ["Frontend Developer", "Backend Developer", "Student"],
+  autoStart: true,
+  loop: true,
+  delay: 100
+});
 
 // Easter Egg
 document.addEventListener('DOMContentLoaded', () => localStorage.removeItem('kirbe'));
 
-onKonamiCode(() => {
+new Egg('up, up, down, down, left, right, left, right, b, a', () => {
   if (localStorage.getItem('kirbe') === '1') return alert('kirbe mode already enabled yes');
   localStorage.setItem('kirbe', 1);
   let audio = new Audio('https://cdn.derpyenterprises.org/website/kirbe.ogg');
@@ -35,13 +30,13 @@ onKonamiCode(() => {
   document.body.style.background = "url('https://cdn.derpyenterprises.org/website/kirby.webp')";
   document.body.style.color = 'white';
   document.title = 'kirbe mode enabled';
-  document.getElementById('icon').src = 'assets/img/kirby.gif';
+  document.getElementById('icon').src = 'https://cdn.derpyenterprises.org/website/kirby.gif';
   document.getElementById('titletext').innerHTML = 'Kirby <3';
   document.getElementById('kirby1').innerHTML = "Enjoyed this Easter egg? Star this repo on GitHub <a href='https://github.com/ohlookitsderpy/derpyenterprises.org' target=_blank'>here</a>!";
   document.getElementById('kirby2').innerHTML = 'The page will automatically refresh once the song is over!';
   document.getElementById('content').style.display = 'none';
   audio.onended = () => { location.reload(); };
-});
+}).listen();
 
 // Wallpapers
 const getCurrentSeason = () => { // https://gist.github.com/neris/5ddff1ec5d421602a01b1c81fa3fc076
@@ -68,6 +63,6 @@ switch (getCurrentSeason()) { // https://gist.github.com/neris/5ddff1ec5d421602a
   case 'autumn':
     document.body.style.background = "url('https://cdn.derpyenterprises.org/website/seasons/autumn.webp')";
     document.getElementById('titletext').setAttribute('style', 'color: white !important;');
-    document.getElementById('gang').setAttribute('style', 'color: white !important;');
+    document.getElementById('text').setAttribute('style', 'color: white !important;');
     break;
 }
